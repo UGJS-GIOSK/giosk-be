8244ca911a9a68c883490e03dfa043600b0090a5
hamgyusang <ebzm00@naver.com>
2025-04-03 09:38:31 +0900

    250403 giosk feat start

---
+++ src/main/java/com/giosk/gioskcafe/GioskcafeApplication.java
@@ -0,0 +1,13 @@
+package com.giosk.gioskcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GioskcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GioskcafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giosk/gioskcafe/GioskcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giosk.gioskcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GioskcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
